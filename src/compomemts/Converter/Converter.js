04f879233e2ffc96a667d4cc21b02309481d6010
Anna <anna4776497@gmail.com>
2022-09-12 21:02:56 +0300

    доделать стили

--- src/compomemts/Converter/Converter.js
+++ src/compomemts/Converter/Converter.js
@@ -3,21 +3,33 @@ import './Converter.css'
 
 const Converter = () => {
     const[form, setForm]=useState({
-        name: ''
-       ,
+        name: '',
+        color: 'white',
         });
        
-
     const handleChange = ({target}) => {
          const {name, value} = target;
          setForm(prevForm => ({...prevForm, [name]: value
-         }))
-         
+         }))  
     }
-    console.log(form.name)
+
+    const hexToRgb = (hex) => {
+        if(hex.length < 7){
+            return
+         }
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? 
+            `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+           : "Ошибка!";
+      }
+
+      const styles = {
+        backgroundColor: hexToRgb(form.name),
+      }
 
     return (
-        <div className="converter">
+        <div className="converter"
+        style={styles}>
           <input className='input-data' 
           id='input-data' 
           name='name'
@@ -25,6 +37,7 @@ const Converter = () => {
           onChange={handleChange}>
           </input>
           <div className='output-data'>
+           {hexToRgb(form.name)}
           </div>
         </div>
     )
